9b3fd5aaed9f0443f7e448c08d4a5ca1856f809e
Cyberistic <Cyb3ristic@gmail.com>
2026-03-16 21:13:41 +0300

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,12 @@
+import { env } from "@enhanced-prisma-studio/env/server";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
+
+import { PrismaClient } from "../prisma/generated/client";
+
+const adapter = new PrismaLibSql({
+  url: env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
